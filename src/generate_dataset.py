6edb5fe5d4f0ccd17b434c2ce42c1370e991880c
SimoneLacatena <berrychain97@gmail.com>
2020-07-04 16:57:58 +0200

    Aggiunti commenti codice generate_dataset.py

--- src/generate_dataset.py
+++ src/generate_dataset.py
@@ -2,17 +2,31 @@
 
 
 import pandas as pd
-import datetime
-
 
 from matplotlib import pyplot as plt
 
 import numpy
 
+import os   
 
 
 
 
+
+"""
+funzione label
+
+input: 
+    annotations : Dataframe contente le annotazioni
+    sample_0 : numero del campione iniziale del segmento
+    sample_f :numero del campione finale del segmento
+
+descrizione:
+    la funzione controlla i vari picchi presenti nel segmento utilizzando annotations
+    e se rileva un anomalia resituisce la label A , se invece non rileva alcuna anomalia restiuisce 
+    la label N
+
+"""
 def label(annotations:pd.DataFrame,sample_0, sample_f):
     r,c= annotations.shape
     start = 0
@@ -31,8 +45,19 @@ def label(annotations:pd.DataFrame,sample_0, sample_f):
     return "N"
     
 
+"""
+funzione createDataset
 
-#rilevare i campioni limite ogni 30 secondi
+input: 
+    annotation : filepath del file contentente le annotazioni
+    samples : filepath del csv contentente i campioni del tracciato
+    dataset_name : filepath del file csv in cui espotare il dataset creato
+    time_treshold:  numero di secondi per segmento
+    sample_rate : frequenza di campionamento con cui è satato campionato i segnale
+
+descrizione:
+    la funzione frammenta il file samples in segmento di time_treshold secondi e gli assegna un etichetta
+"""
 def createDataset(annotation:str,samples:str,dataset_name:str,time_treshold=30,sample_rate = 360):
     annotations = pd.read_csv(annotation)
     data = pd.read_csv(samples)
@@ -75,53 +100,39 @@ def createDataset(annotation:str,samples:str,dataset_name:str,time_treshold=30,s
     
     
 
-
-#dataset = pd.read_csv(filepath + 'Dataset.csv')
-
-
-
-
-
-
-def channels(segment_index,dataset):
-    ch0= []
-    ch1= []
-    i = 0
-    keys = dataset.keys()
-    values = dataset.loc[segment_index]
-    while i < len(values) -2:
-        if(keys[i] != 'label'):
-            ch0.append(values[i])
-            ch1.append(values[i+1])
-            i+=2
-        else :
-            i+=1
-             
-    return ch0,ch1
-
-
+"""
 def plot(datafilename,segment_index,start=0,end=3,f = 0.00278):
     dataset = pd.read_csv(datafilename)
-    ch0,ch1 = channels(segment_index,dataset)
-    
-    x = numpy.arange(0,len(ch0),f)
-    x = x[:len(ch0)]
+    y = list(dataset.loc[segment_index])
+    x = numpy.arange(0,len(y),f)
+    x = x[:len(y)]
     plt.xlim(start, end)
     plt.ylim(0, 3000)
-    
-    plt.plot(x,ch0)  
-    plt.plot(x,ch1)
+    plt.plot(x,y)  
     plt.show()
     
 
-    
-import os   
+""" 
 
 
+
+"""
+funzione extract_data
+
+input: 
+    filepart : filepath della cartella dove si trovano i files contenti i campioni e le annotazioni
+    outputDir : filepath del cartalle in cui memorizzare i csv
+
+descrizione:
+    la funzione processa i dati presenti il filepath e resituisce dei csv uno per ogni coppia di file 
+    es. 100,100annotations  -> data100
+
+"""
 
 def extract_data(filepath,outputDir):
    # filepath  = '.\\Dataset\\mitbih_database\\files_preprocessed'
    # outputDir  = '.\\Dataset\\mitbih_database\\dataset'
+    
     files = os.listdir(filepath)
     
     i = 0
@@ -134,7 +145,30 @@ def extract_data(filepath,outputDir):
         createDataset(filepath +"\\"+annotations,filepath +"\\"+samples,outputDir+"\\data"+samples)
         i+=2
         
+"""
+funzione union
 
+input: 
+    directorypath : filepath della cartella dove si trovano i csv da unire
+    utputdir: fielpath della cortella che conterra l'unic csv'
+
+descrizione:
+    la funzione unisce tutti csv presenti in un unico csv
+
+"""
+def union(directorypath:str,outputdir:str):
+    data = os.listdir(directorypath)
+    dataset =  pd.read_csv(directorypath +"\\" + data[0],index_col=0)
+    for i in range(1,len(data)):
+        print(data[i])
+        data_i  = pd.read_csv(directorypath +"\\" + data[i],index_col=0)
+        dataset = pd.concat([dataset, data_i], ignore_index=True)
+        
+    
+    dataset.to_csv(outputdir + "\\end_dataset.csv",index=False)
+        
+        
+    
 
 
 
